b94cdcc1ef8e37895492420651dc6232ad5f4c4e
inipro <info@inipro.net>
2023-03-08 13:34:25 +0900

    Update ander.vhd

--- src/ander.vhd
+++ src/ander.vhd
@@ -1,11 +1,11 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity ander is   
-  port(
-    a,b : in std_logic;
-    result : out std_logic
-  );
+entity ander is
+	port(
+		a,b : in std_logic;
+		result : out std_logic
+	);
 end ander;
 
 architecture structural of ander is
