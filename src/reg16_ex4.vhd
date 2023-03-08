490390b4efb32e62a82362638efdf31318c04b26
inipro <info@inipro.net>
2023-03-08 16:31:59 +0900

    Update reg16_ex4.vhd

--- src/reg16_ex4.vhd
+++ src/reg16_ex4.vhd
@@ -2,21 +2,21 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity reg16_ex4 is
-  port(
-    clk, a : in std_logic;        
-    d : out std_logic
-  );  
+	port(
+		clk, a : in std_logic;
+		d : out std_logic
+	);
 end reg16_ex4;
 
 architecture Behavioral of reg16_ex4 is
-  signal b,c : std_logic;
+	signal b,c : std_logic;
 begin
-  process(clk)
-  begin
-    if rising_edge(clk) then
-      b <= a;
-      c <= b;
-      d <= c;
-    end if;
-  end process;
+	process(clk)
+	begin
+		if rising_edge(clk) then
+			b <= a;
+			c <= b;
+			d <= c;
+		end if;
+	end process;
 end Behavioral;
