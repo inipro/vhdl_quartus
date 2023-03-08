d8261cb10bfb26055665b34cbc75f6adc26546d2
inipro <info@inipro.net>
2023-03-08 16:39:28 +0900

    Update reg16_ex5.vhd

--- src/reg16_ex5.vhd
+++ src/reg16_ex5.vhd
@@ -2,21 +2,21 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity reg16_ex5 is
-  port(
-    clk, a : in std_logic;        
-    d : out std_logic
-  );  
+	port(
+		clk, a : in std_logic;
+		d : out std_logic
+	);
 end reg16_ex5;
 
-architecture Behavioral of reg16_ex5 is    
+architecture Behavioral of reg16_ex5 is
 begin
-  process(clk)
-    variable b,c : std_logic;
-  begin
-    if rising_edge(clk) then
-      b := a;
-      c := b;
-      d <= c;
-    end if;
-  end process;
+	process(clk)
+		variable b,c : std_logic;
+	begin
+		if rising_edge(clk) then
+			b := a;
+			c := b;
+			d <= c;
+		end if;
+	end process;
 end Behavioral;
