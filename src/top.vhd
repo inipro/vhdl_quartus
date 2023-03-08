f12d5664bb2553cded6fa275d4ad869473ea9f9b
inipro <info@inipro.net>
2023-03-08 15:51:22 +0900

    Update top.vhd

--- src/top.vhd
+++ src/top.vhd
@@ -4,22 +4,22 @@ use ieee.std_logic_1164.all;
 entity top is
 	port(
 		clk : in std_logic;
-    		q : out std_logic_vector(31 downto 0)
+		q : out std_logic_vector(31 downto 0)
 	);
 end top;
 	
 architecture structural of top is
 
-	component cntr is    
+	component cnt is
 	Port(
 		clock : in std_logic;
-     		q : out std_logic_vector(31 downto 0)
+		q : out std_logic_vector(31 downto 0)
 	);
 	end component;
   
 begin	
 	
-	cntr_inst : cntr PORT MAP (
+	cnt_inst : cnt PORT MAP (
 		clock	 => clk,
 		q	 => q
 	);
